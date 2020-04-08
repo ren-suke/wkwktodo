1a3c1ec94e4b1002b2ff41e157b57e7bd0392701
RenMatsushita <ren.git.x@gmail.com>
2020-04-08 19:00:58 +0900

    replace作業のため保存

---
+++ wkwktodo/View/Home/ContentView/WPView.swift
@@ -0,0 +1,21 @@
+//
+//  WPView.swift
+//  wkwktodo
+//
+//  Created by Ren Matsushita on 2020/04/08.
+//  Copyright © 2020 Ren Matsushita. All rights reserved.
+//
+
+import UIKit
+
+class WPView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ wkwktodo/View/Home/OriginalListSectionHeaderCollectionReusableView.swift
@@ -0,0 +1,18 @@
+//
+//  OriginalListSectionHeaderCollectionReusableView.swift
+//  wkwktodo
+//
+//  Created by Ren Matsushita on 2020/04/08.
+//  Copyright © 2020 Ren Matsushita. All rights reserved.
+//
+
+import UIKit
+
+class OriginalListSectionHeaderCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
